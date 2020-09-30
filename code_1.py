5ac662c71e0f8c56043ff170d63c5711a006661a
vnegs14 <71794393+vnegs14@users.noreply.github.com>
2020-09-30 17:50:01 -0400

    average file before loop

--- code_1.py
+++ code_1.py
@@ -1,2 +1,18 @@
-numberOfScores = input("Please enter the number of scores you want to average: ")
-print(numberOfScores)
+#intialize all variables to 0
+numberOfScores = 0
+score = 0
+total = 0
+ccoreCount = 0
+average = 0.0
+
+#Accept the number of scores to average
+numberOfScores = int(input("Please enter the number of scores you want to average: "))
+
+#Add a loop to make this code repeat until scoreCount = numberOfScores
+score = int(input("Please enter a score: "))
+total = total + score
+scoreCount = scoreCount + 1
+
+
+average = total / numberOfScores
+
