2c51c2ef689f427684daf0c2a8347368514e2690
vnegs14 <71794393+vnegs14@users.noreply.github.com>
2020-10-26 17:42:13 -0400

    For Loop assignment

---
+++ ForLoop.py
@@ -0,0 +1,7 @@
+x = 1
+print("The program will start counting up from 0 and finish at 9.")
+print("You will recieve a nice message after the program has ended.")
+response=(input("Are you ready to proceed? "))
+for x in range(0,10):
+    print ("total= ",x)
+print("Program has ended folks! Take care and Stay Frosty - Vinnie")    
