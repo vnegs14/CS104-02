9ba492b6be5ba36151dd79ebf2aa0d4499a19b41
vnegs14 <71794393+vnegs14@users.noreply.github.com>
2020-11-25 11:08:12 -0500

    The Bank Simulation

---
+++ PythonQueue.py
@@ -0,0 +1,13 @@
+names = ["Vinnie", "Jack", "Robert", "Brittani", "Shelby", "Mia", "Sarah", "Spencer", "Tom", "Alissa"]
+queue=[]
+acceptedName=print
+names.append(acceptedName)
+for acceptedName in names:
+    print(acceptedName)
+len(names)
+for acceptedName in names:
+    deque=[]
+    print(names.pop(0))
+    print(names.pop(0))
+    print(names.pop(0))
+print(names)
